8d3de996038c1b83c419ec565a982072fc447e90
Mëgá Bytē <frankdanil021706@gmail.com>
2026-05-08 18:04:35 +0100

    Set up Express server for Megabyte Bot

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Megabyte Bot is running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 /**
  * WhatsApp MD Bot - Main Entry Point
  */
